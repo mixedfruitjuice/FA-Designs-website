9eb9b1a621a40b7e8557ec8eb37a94aefc1eb59f
yannick <yannickforget@live.nl>
2016-08-14 21:18:11 +0200

    added some services to the home page

--- public/js/script.js
+++ public/js/script.js
@@ -1,27 +1,29 @@
 
 $( document ).ready(function() {
+    if ((screen.width<=1024) && (screen.height<=768)) {
+        $('#geschikt1').css('display', 'block');
+        $('#geschikt2').css('display', 'block');
+        $('.service').css('display', 'block');
+    }
     $(window).scroll(function() {
+
           if ((screen.width>=1024) && (screen.height>=768)) {
               if ($(document).scrollTop() > 50) {
-                $('.underbar').addClass('shrink');
-                $("#navlogo").addClass('shrinkimg');
-                $('.topbar').css('display', 'none');
+                  $('.underbar').addClass('shrink');
+                  $("#navlogo").addClass('shrinkimg');
+                  $('.topbar').css('display', 'none');
               } else {
-                $('.underbar').removeClass('shrink');
-                $('#navlogo').removeClass('shrinkimg');
-                $('.topbar').css('display', 'block');
+                  $('.underbar').removeClass('shrink');
+                  $('#navlogo').removeClass('shrinkimg');
+                  $('.topbar').css('display', 'block');
               }
           }
+
+
           var y = $(this).scrollTop();
           if (y > 300) {
               $('#geschikt1').fadeIn(1000);
               $('#geschikt1').css('display', 'block');
-
-          }
-
-          if (y > 360) {
-              $('#geschikt2').fadeIn(1000);
-              $('#geschikt2').css('display', 'block');
           }
 
           if (y > 360) {
